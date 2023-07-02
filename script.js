f4c8a3cf2ccfc1a2a707d5389b72647363221404
nirbhay-vora <nirbhayvora9615@gmail.com>
2023-07-02 10:26:28 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+
+    let hrotation = 30*htime + mtime/2;
+    let mrotation  = mtime*6;
+    let srotation = stime*6;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
